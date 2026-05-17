b9abe65f77bd5080912ae854bf12e14ad65475bc
nathbiswa <biswanroy29@gmail.com>
2026-05-17 22:08:28 +0600

    mongodb added done

--- index.js
+++ index.js
@@ -12,6 +12,47 @@ const PORT = process.env.PORT || 8000
 app.use(cors());
 app.use(express.json());
 
+// Mongodb start here
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Create Database
+        const db = client.db('healthcare')
+        const doctorsCollection = db.collection('doclist')
+
+        // Find all doctor list
+        app.get('/doclist', async (req, res) => {
+            const result = await doctorsCollection.find().toArray();
+            console.log(result);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
